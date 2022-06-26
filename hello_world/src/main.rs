3b3e91564b3483e56205765e9abefa7c236c6a65
eager7 <panchangtao@gmail.com>
2022-06-27 00:05:46 +0800

    grep

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,11 @@
 use hello_world;
 use std::cell::Cell;
 use std::fmt;
+use restaurant;
 
 fn main() {
     println!("Hello, world!");
     hello_world::hello::hello();
+
+    restaurant::eat_at_restaurant();
 }
--- mgrep/src/main.rs
+++ mgrep/src/main.rs
@@ -1,14 +1,14 @@
-use mgrep;
+use pct_grep;
 use std::{env, process};
 
 fn main() {
     println!("Hello, mgrep!");
-    let config = mgrep::Config::new(env::args()).unwrap_or_else(|err| {
+    let config = pct_grep::Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("config err:{}", err);
         process::exit(1);
     });
 
-    if let Err(e) = mgrep::run(config) {
+    if let Err(e) = pct_grep::run(config) {
         eprintln!("app err:{}", e);
         process::exit(1);
     }
