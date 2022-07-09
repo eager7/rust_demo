29232c277c3cd7dddd0fe397ab93d53326c3d026
eager7 <panchangtao@gmail.com>
2022-07-09 11:45:11 +0800

    lifetime

--- hello_world/src/lifetime.rs
+++ hello_world/src/lifetime.rs
@@ -20,3 +20,26 @@ fn longest_example() {
     let result = longest(&s1, &s2);
     println!("{}", result);
 }
+
+fn copy_and_return<'a>(vector: &mut Vec<String>, value: &'a str)->&'a str {
+    vector.push(String::from(value));
+    value
+}
+
+#[test]
+fn life_ex() {
+    let name1 = "Joe";
+    let name2 = "Chris";
+    let name3 = "Anne";
+
+    let mut names = Vec::new();
+
+    assert_eq!("Joe", copy_and_return(&mut names, &name1));
+    assert_eq!("Chris", copy_and_return(&mut names, &name2));
+    assert_eq!("Anne", copy_and_return(&mut names, &name3));
+
+    assert_eq!(
+        names,
+        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
+    )
+}
